657c12af47989ee01d2c5b3116550af16b8e1443
Karthik Goud Masagoni <karthikmasagoni18@gmail.com>
2026-06-22 22:44:55 +0530

    Add files via upload

--- groq_api.py
+++ groq_api.py
@@ -1,9 +1,7 @@
+import streamlit as st
 from groq import Groq
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
-client=Groq(api_key="GROQ_API_KEY")
+client=Groq(api_key=st.secrets["GROQ_API_KEY"])
 
 
 def get_ai_feedback(
